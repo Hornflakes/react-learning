be01d7e9bf153df409c920e25ddd1b132fa980b8
Hornflakes <cristianpetrugug@gmail.com>
2026-04-10 19:31:32 +0300

    chore: tsconfig, gitignore, prettier, eslint, husky, .vscode settings, alias paths & ant favicon

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import js from '@eslint/js';
-import globals from 'globals';
+import importPlugin from 'eslint-plugin-import';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
     globalIgnores(['dist']),
@@ -15,9 +16,40 @@ export default defineConfig([
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
         ],
+        plugins: {
+            import: importPlugin,
+        },
         languageOptions: {
-            ecmaVersion: 2020,
             globals: globals.browser,
+        },
+        rules: {
+            'no-restricted-imports': [
+                'error',
+                {
+                    patterns: ['../*', './../*'],
+                },
+            ],
+
+            '@typescript-eslint/consistent-type-imports': [
+                'error',
+                {
+                    prefer: 'type-imports',
+                    fixStyle: 'inline-type-imports',
+                },
+            ],
+
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+
+            '@typescript-eslint/no-namespace': 'off',
+
+            'import/no-duplicates': 'error',
         },
     },
 ]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Heading } from '@components';
+
 export const App = () => {
-    return <h1>Hello, World!</h1>;
+    return <Heading text="Hello, World!" />;
 };
