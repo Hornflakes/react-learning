3695ed740c36c7d6ee0e284ddda3770698372755
Hornflakes <cristianpetrugug@gmail.com>
2026-04-12 22:03:59 +0300

    feat: effect resource - show pending only after one second when refetching

--- src/components/EffectAsync.tsx
+++ src/components/EffectAsync.tsx
@@ -1,4 +1,5 @@
 import type { EffectResource } from '@hooks';
+import { useEffect, useState } from 'react';
 
 type EffectAsnycProps<R> = {
     resource: EffectResource<R>;
@@ -9,5 +10,20 @@ type EffectAsnycProps<R> = {
 export const EffectAsync = <R,>({ resource, pending, ready, errored }: EffectAsnycProps<R>) => {
     const { data, state, error } = resource;
 
-    return <>{state === 'pending' ? pending : error ? errored(error) : ready(data)}</>;
+    const [showRefreshing, setShowRefreshing] = useState(false);
+
+    useEffect(() => {
+        if (state !== 'refreshing') return;
+
+        const timer = setTimeout(() => setShowRefreshing(true), 1000);
+        return () => {
+            setShowRefreshing(false);
+            clearTimeout(timer);
+        };
+    }, [state]);
+
+    if (state === 'pending') return pending;
+    if (state === 'refreshing' && showRefreshing) return pending;
+    if (state === 'errored' && error) return errored(error);
+    return ready(data);
 };
